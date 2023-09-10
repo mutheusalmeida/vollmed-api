e518e4317f535e5e858984ef5e94d42d23eca28f
Matheus Almeida <matheusalmeida016@gmail.com>
2023-09-10 15:44:26 -0400

    feat: add project setup and make first request

---
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -0,0 +1,17 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/doctors")
+public class DoctorController {
+
+	@PostMapping
+	public void register (@RequestBody String req) {
+		System.out.println(req);
+	}
+
+}
